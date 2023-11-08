4185e593a32887a43d43ce8fa88547a639232f0c
GeMixl <g.michl@gmx.net>
2023-11-08 22:23:48 +0100

    Solved day 10 second part

--- day10/day10.py
+++ day10/day10.py
@@ -14,20 +14,34 @@ def read_input(filename: str) -> []:
                  for line in fs]
     return input
 
+def render_screen(scn):
+    lines = ["".join(scn[i:i+40]) for i in range(0, len(scn)-1, 40)]
+    return "\n".join(lines)
 
 def solve_part_i(input):
     cycles = np.cumsum(np.array([i[2] for i in input]))
-    strength = np.cumsum(np.array([i[1] for i in input])) + 1
+    strength = 1 + np.cumsum(np.array([i[1] for i in input]))
     evaluation_intervals = [i for i in range(20, cycles[-1], 40)]
-    strength_fluctuations = np.stack((np.array([i for i in range(len(cycles))]), cycles, strength), axis=1)
-    print(evaluation_intervals)
-    cycle_intervals = np.searchsorted(cycles, evaluation_intervals, side='left')-1
-    print(cycle_intervals)
+    cycle_intervals = np.searchsorted(cycles, evaluation_intervals, side='left') - 1
     return sum(strength[cycle_intervals] * evaluation_intervals)
 
 def solve_part_ii(input):
-    pass
-
+    screen = ["."] * sum([i[2] for i in input])
+    pixel_position = 0
+    sprite_offset = 0
+    sprite_position = 1
+    for _, jmp, cyc in input:
+        for cycle in range(cyc):
+            if sprite_offset + sprite_position - 1 == pixel_position:
+                screen[pixel_position] = "#"
+            if sprite_offset + sprite_position == pixel_position:
+                screen[pixel_position] = "#"
+            if sprite_offset + sprite_position + 1 == pixel_position:
+                screen[pixel_position] = "#"
+            pixel_position += 1
+        sprite_position += jmp
+        sprite_offset = 40 * (pixel_position//40)
+    return render_screen(screen)
 
 if __name__ == "__main__":
     filename = "input_day10.txt"
